048654f08c29cd96dde3b928bc59495f4c08f78f
ruslan <gitanozg@.gmail.com>
2018-05-16 16:36:09 -0400

    Add print line

--- src/test/java/Test/Google.java
+++ src/test/java/Test/Google.java
@@ -5,10 +5,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class Google {
-
+WebDriver driver;
 	@Test
 	public void testGoogle() {
 		System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver.exe");
@@ -18,6 +19,11 @@ public class Google {
 		driver.findElement(By.name("q")).sendKeys("Kia"+Keys.ENTER);
 		String expected="Kia";
 		String actual=driver.getTitle();
+		System.out.println("actual is: "+actual);
 		Assert.assertTrue(actual.contains(expected));
+	}
+	@AfterMethod
+	public void after() {
+		driver.close();
 	}
 }
